fb468487ef8fec7cbfd81d62233f8c631ac299e0
gabriel03013 <gm5770283@gmail.com>
2026-05-30 16:12:07 -0300

    fix: add redis connection

--- src/main/java/com/frigus/coreapi/CoreApiApplication.java
+++ src/main/java/com/frigus/coreapi/CoreApiApplication.java
@@ -3,14 +3,10 @@ package com.frigus.coreapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// google cloud e redis desativados temporiaramente para o projeto rodar, depois adicionaremos
+// google cloud desativado temporiaramente para o projeto rodar, depois adicionaremos
 @SpringBootApplication(excludeName = {
         "com.google.cloud.spring.autoconfigure.core.GcpContextAutoConfiguration",
         "com.google.cloud.spring.autoconfigure.storage.GcpStorageAutoConfiguration",
-
-        "org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration",
-        "org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration",
-        "org.springframework.boot.autoconfigure.session.SessionAutoConfiguration"
 })
 public class CoreApiApplication {
 
