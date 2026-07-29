ecc0bbcf14b8eaa440f7e81d8c8e4f859d7b018b
gabriel03013 <gm5770283@gmail.com>
2026-07-29 20:08:20 -0300

    chore: add service utils helper and format date in user dtos

--- src/main/java/com/frigus/coreapi/dto/user/UserRegisterRequestDto.java
+++ src/main/java/com/frigus/coreapi/dto/user/UserRegisterRequestDto.java
@@ -1,5 +1,7 @@
 package com.frigus.coreapi.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -10,8 +12,22 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRegisterRequestDto {
+    @NotBlank(message = "O nome é obrigatório")
     private String name;
+
+    @NotNull(message = "A data de nascimento é obrigatória")
+    @Past(message = "A data de nascimento deve ser no passado")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate birthDate;
+
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "Email inválido")
     private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,20}$",
+        message = "A senha deve conter de 8 a 20 caracteres, contendo pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial"
+    )
     private String rawPassword;
 }
