4ae75a21684debc28fb9d0462bdd891056acaff8
GustavoManganelli <gugu.manganeli@gmail.com>
2026-08-24 06:09:38 -0600

    Revert "feat(discard): add discard API classes"

--- src/main/java/com/frigus/coreapi/repository/DiscardRepository.java
+++
@@ -1,6 +0,0 @@
-package com.frigus.coreapi.repository;
-
-import com.frigus.coreapi.model.Discard;
-
-public interface DiscardRepository extends BaseRepository<Discard, Integer>{
-}
--- src/main/java/com/frigus/coreapi/repository/StockProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.frigus.coreapi.repository;
-
-import com.frigus.coreapi.model.StockProduct;
-
-public interface StockProductRepository extends BaseRepository<StockProduct, Integer> {
-}
